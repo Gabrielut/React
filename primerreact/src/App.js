de770f52f7148af369361a48a1f7468b37796b67
Gabriel Torres <gtorress106@gmail.com>
2020-09-06 20:55:04 -0600

    diseño plantilla
    se crea una plantilla de cursos, y se muestran 3 diferentes formas de pasar informacion

--- primerreact/src/App.js
+++ primerreact/src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import headerImage from './header.jpg';
+import cursoImage from './cursos.jpg';
+import './index.css';
+
+// Los siguientes 3 hacen lo mismo y son 3 formas diferentes de hacerlo
+// Componente funcion
+function Course(props) {
+  return (
+
+    <div className="course">
+      <img src={cursoImage}></img>
+      <h3>{props.titulo}</h3>
+      <a href={props.link}>Ver Curso</a>
+    </div>
+  );
+};
+
+// Componente arrow function
+// const Course = (props) =>{
+//   return (
+//     <div className="course">
+//       <img src={cursoImage}></img>
+//       <h3>{props.titulo}</h3>
+//       <a href={props.link}>Ver Curso</a>
+//     </div>
+//   );
+// }
+
+// Componente de clase
+// class Course extends React.Component{
+// render(){
+//     return (
+//     <div className="course">
+//       <img src={cursoImage}></img>
+//       <h3>{this.props.titulo}</h3>
+//       <a href={this.props.link}>Ver Curso</a>
+//     </div>
+//   );
+// }
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Bienvenidos a nuestros cursos</h1>
       </header>
+      <section className="courses">
+        <Course titulo="Curso de desarrollo web" link="/web"></Course>
+        <Course titulo="Curso de js" link="/js"></Course>
+        <Course titulo="Curso de Html" link="/html"></Course>
+
+      </section>
+      <footer>
+        <p>Todos los derechos reservados</p>
+      </footer>
     </div>
   );
 }
